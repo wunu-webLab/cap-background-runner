3b78983f1795ede49aa06756997be93c4763e09b
Florian Sojer <75686087+FloSojer@users.noreply.github.com>
2024-11-12 12:45:36 +0100

    Changed Noptifications to be Clickable

--- packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/api/Notifications.kt
+++ packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/api/Notifications.kt
@@ -82,8 +82,11 @@ class Notifications(context: Context) : NotificationsAPI {
 
         notifications.forEach {
             val channelId = it.channelId ?: defaultNotificationChannelID
-
+            val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+            intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
             val builder = NotificationCompat.Builder(context, channelId)
+            builder.setContentIntent(pendingIntent)
             builder.setContentTitle(it.title)
             builder.setContentText(it.body)
             builder.setAutoCancel(it.autoCancel)
